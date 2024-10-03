9a6ee87ae10ec4299f49aea8af4e0eba182ea65e
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2024-10-03 17:39:13 +0200

    add basic function

--- go-basics/investment_calculator.go
+++ go-basics/investment_calculator.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+
+	var futureValue = math.Pow(float64(investmentAmount)*(1+expectedReturnRate/100), float64(years))
+
+	fmt.Println(futureValue)
 }
