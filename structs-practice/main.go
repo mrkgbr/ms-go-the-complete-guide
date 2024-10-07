39b3f65a7d2c7e27c4886cbc45a1d22f93ebaccc
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2024-10-07 17:30:39 +0200

    finished section 6

--- structs-practice/main.go
+++ structs-practice/main.go
@@ -7,10 +7,36 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
 
+type saver interface {
+	Save() error
+}
+
+// type displayer interface {
+// 	Display()
+// }
+
+type outputtable interface {
+	saver
+	Display()
+}
+
 func main() {
+	printSomething(1)
+	printSomething(1.5)
+	printSomething("something")
+
 	title, content := getNoteData()
+	todoText := getUserInput("Todo text: ")
+
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	userNote, err := note.New(title, content)
 
@@ -19,15 +45,47 @@ func main() {
 		return
 	}
 
-	userNote.Display()
-	err = userNote.Save()
-
+	err = outputData(todo)
 	if err != nil {
-		fmt.Println("Saving the note failed!")
 		return
 	}
 
+	outputData(userNote)
+}
+
+func printSomething(value interface{}) {
+	// typedVal, ok := value.(int)
+	// if ok {
+	// 	fmt.Println("Integer:", value)
+	//	return
+	// }
+	// ...
+
+	switch value.(type) {
+	case int:
+		fmt.Println("Integer:", value)
+	case float64:
+		fmt.Println("Float64:", value)
+	case string:
+		fmt.Println(value)
+	}
+}
+
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
+}
+
+func saveData(data saver) error {
+	err := data.Save()
+
+	if err != nil {
+		fmt.Println("Saving the note failed!")
+		return err
+	}
+
 	fmt.Println("Saving the note succeeded!")
+	return nil
 }
 
 func getNoteData() (string, string) {
